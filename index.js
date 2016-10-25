1c0e97cff5704ae38390351eee162099f2a6acf2
Chuck Harmston <chuck@mozilla.com>
2016-10-24 20:01:10 -0600

    Adds 'Search on Scryfall' to results.

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
 'use strict';
 const alfy = require('alfy');
 
+const API_URL = `https://api.scryfall.com/cards/search?q=include%3Aextras+${encodeURI(alfy.input)}`;
+const SEARCH_URL = `https://www.scryfall.com/search?q=include%3Aextras+${encodeURI(alfy.input)}`;
+
 const EMPTY = [{
   title: '¯\\_(ツ)_/¯',
   subtitle: `Hellbent: no cards matching "${alfy.input}".`,
-  valid: false
+  arg: SEARCH_URL,
+  quicklookurl: SEARCH_URL
 }];
-const ONE_HOUR = 1000 * 60 * 60;
-const SCRYFALL = `https://api.scryfall.com/cards/search?q=include%3Aextras+${encodeURI(alfy.input)}`;
+const SEARCH = [{
+  uid: `on-site-${alfy.input}`,
+  title: 'Search on Scryfall',
+  arg: SEARCH_URL,
+  quicklookurl: SEARCH_URL
+}];
 
-alfy.fetch(SCRYFALL, { maxAge: ONE_HOUR })
+const ONE_HOUR = 1000 * 60 * 60;
+
+const formatCard = card => {
+  const output = {
+    uid: card.id,
+    title: card.name,
+    subtitle: card.type_line,
+    arg: card.scryfall_uri,
+    quicklookurl: card.scryfall_uri,
+    autocomplete: card.name
+  };
+  if (card.name === 'Amoeboid Changeling') {
+    output.icon = { path: 'img/o.O.jpg' };
+  }
+  return output;
+}
+
+alfy.fetch(API_URL, { maxAge: ONE_HOUR })
   .then(results => {
     if (results.object === "error") {
       alfy.output(EMPTY);
     } else {
-      alfy.output(results.data.slice(0, 10).map(card => ({
-        uid: card.id,
-        title: card.name,
-        subtitle: card.type_line,
-        arg: card.scryfall_uri,
-        quicklookurl: card.scryfall_uri,
-        autocomplete: card.name
-      })));
+      alfy.output(SEARCH.concat(results.data.slice(0, 10).map(formatCard)));
     }
   })
   .catch(err => {
